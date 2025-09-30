56be4f62d019507b04952dee8eb8e2e4e0717946
tjgus2 <162402315+tjgus2@users.noreply.github.com>
2025-09-30 23:58:21 +0900

    final

--- script/mainText.js
+++ script/mainText.js
@@ -4,21 +4,18 @@ const mainText = document.querySelectorAll(".text_line");
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // 요소가 보일 때 실행
             mainText.forEach(content => {
                 content.classList.add("word");
             });
         } else {
-            // 요소가 안 보일 때 실행 (다시 올라가면 초기화)
             mainText.forEach(content => {
                 content.classList.remove("word");
             });
         }
     });
 }, {
-    threshold: 0.1 // 요소가 10% 이상 보이면 트리거
+    threshold: 0.1
 });
 
-// mainTextContent 요소 관찰 시작
 observer.observe(mainTextContent);
 
